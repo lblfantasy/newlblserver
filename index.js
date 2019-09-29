da21b851b07b229d0b8166ec9eacc92212e143b4
lblfantasy <lebfantasy@gmail.com>
2019-09-29 12:04:57 +0300

    Update index.js

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ var api = new ParseServer({
       apiKey: 'AIzaSyB_2yWQmTn04usSEQWr_hq1ca987xTiOEY' // The Server API Key of GCM
     },
      ios: {
-        pfx: 'Certificate_LBL.p12',
+        pfx: 'CertificateLBLProduction.p12',
         passphrase: '', // optional password to your p12/PFX
         bundleId: 'BRICS.LBLFantasyLeagues',
         production: true
